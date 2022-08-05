15fe93d3c204608d38502dc376d15dc0cb283c15
pradeep_solanki <pradeepsolanki0799@gmial.com>
2022-08-05 19:20:14 +0530

    modify both file

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,15 @@
 print("hello world")
 print('Hii')
+# git clone https://github.com/arenjre/learn-git.git
+#  git chechout -b pradeep
+#  git branch
+#  git add .
+# git commit -m "this is first commit"
+# git push origin pradeep
+
+
+# modifeid
+# git add
+# git commit
+# git pull
+# git push
--- practice.py
+++ practice.py
@@ -1,3 +1,18 @@
 print('This is learning fire')
 for i in range(5):
     print(i)
+
+
+# git clone https://github.com/arenjre/learn-git.git
+#  git chechout -b pradeep
+#  git branch
+#  git add .
+# git commit -m "this is first commit"
+# git push origin pradeep
+
+
+# modifeid
+# git add
+# git commit
+# git pull
+# git push
