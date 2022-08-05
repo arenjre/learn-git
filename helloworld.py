a41ff858d106bdb4283a07f5db67f1400711674a
pradeep_solanki <pradeepsolanki0799@gmial.com>
2022-08-05 19:28:29 +0530

    modify  file

--- helloworld.py
+++ helloworld.py
@@ -1,26 +1,2 @@
-<<<<<<< HEAD
 print("hello world")
 print('Hii')
-# git clone https://github.com/arenjre/learn-git.git
-#  git chechout -b pradeep
-#  git branch
-#  git add .
-# git commit -m "this is first commit"
-# git push origin pradeep
-
-
-# modifeid
-# git add
-# git commit
-# git pull
-# git push
-=======
-print("hello        world")
-
-
-
-
-
-
-print('Hii')
->>>>>>> f6434f9d1fa2d5728fb96ccc670f8994b0b94008
