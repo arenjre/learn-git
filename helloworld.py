089a6c52a6051d343c7dd1ce7d949a761d41fd59
Vishal Patidar <vishupatidar10@gmail.com>
2022-08-08 19:49:25 +0530

    jhf

--- helloworld.py
+++ helloworld.py
@@ -10,7 +10,6 @@ print("hello        world")
 print("hello world............. ")
 print("hello world............. ")
 print("hello world............. ")
-print("hello world............. ")
 print("hello world")
 
 
